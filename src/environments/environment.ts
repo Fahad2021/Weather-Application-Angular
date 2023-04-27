eb6693c4f1522351ae34e4d3dc99f91b2f5a11a1
fahad <fahadrahman810@gmail.com>
2023-04-27 08:42:11 +0600

    adding api

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: false,
+    weatherApiBaseUrl:'https://weatherinfo.p.rapidapi.com/api/weather',
+    XRapidAPIHostHeaderName:'X-RapidAPI-Host',
+    XRapidAPIHostHeadervalue:'weatherinfo.p.rapidapi.com',
+
+    XRapidAPIKeyHostHeaderName:'X-RapidAPI-Key',
+    XRapidAPIKeyHostHeaderValue:'2adc5f1cf6msh264d6708aaa4316p1d2c7djsn8e0c20c122d4'
+};
